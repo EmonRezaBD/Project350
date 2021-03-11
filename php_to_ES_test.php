6ca789f8a4de1a9934a5489b622de812bd28a483
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2021-03-11 15:28:24 +0600

    Update php_to_ES_test.php

--- php_to_ES_test.php
+++ php_to_ES_test.php
@@ -3,7 +3,10 @@
 use Elasticsearch\ClientBuilder;
 require 'vendor/autoload.php';
 
-$client = ClientBuilder::create()->build();
+$client = ClientBuilder::create()
+  ->setHosts(['localhost:9200'])
+  ->build();
+  
 echo "ES client has been created.";
 
 
