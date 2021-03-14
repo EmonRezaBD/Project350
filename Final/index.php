056a79a989aa9400c585ea0cb094e1df2b92780f
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2021-03-14 17:37:26 +0600

    insert and search Ok

--- Final/index.php
+++ Final/index.php
@@ -1,18 +1,6 @@
 <?php
-    require_once 'app/init.php';
 
-    if(isset($_GET['q']))
-    {
-        echo 'ALhamdulliah';
-        $q = $_GET['q'];
-
-        $query = $client -> search([
-
-                
-
-        ]);
-
-    }
+require_once 'app/init.php';
 
 
 ?>
@@ -26,12 +14,71 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    <form action="index.php" method="get" autocomplete="off">
+    <form action="/elasticsearch-7.11.1/esphpsearch/index.php" method="POST" autocomplete="off">
         <label>
             Search for something
             <input type="text" name="q">
         </label>
         <input type="submit" value="Search">
     </form>
+
+    
+   <?php
+
+if(!empty($_POST)) {
+    $q = $_POST['q'];
+    $params = [
+        'index' => 'articles',
+        'body'  => [
+            'query' => [
+                'match' => [
+                    'keywords' => $q
+                ]
+            ]
+        ]
+    ];
+    
+    $result = $client->search($params);
+    print_r($result);
+}
+
+
+
+        // echo $query['_source'] ['title'];
+        //echo "Array: <br>";
+        
+        // foreach($query['_id'] as $result)
+        // {
+        //     echo $result['_id'], '<br>';
+        // }
+
+       // print_r($query);
+        // echo $query[article[_id]];
+
+       // if(isset($results))
+      //  {
+            // foreach($results as $r)
+            // {
+               /* ?>
+                
+                   <div class ="result">
+                      <a href = "#<?php echo $r['_id']; ?>" > <?php echo $r['_source'] ['title']; ?> </a>
+                      <div class = "result-keywords"> <?php implode(', ', $r['_source']['keywords']); ?>
+                  </div>
+                  
+
+               <?php */
+
+              
+            // }
+       // }
+            //var_dump($results);
+            //foreach($results as $r)
+            //{
+            //    var_dump($r);
+            //}
+
+    ?>
+
 </body>
 </html>
